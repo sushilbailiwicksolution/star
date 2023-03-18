79c24f9b280b2b9a7813c9c53a05395747940026
sushilbailiwicksolution <sushil@bailiwicksolution.com>
2023-03-18 14:08:02 +0530

    removing old code

--- NEST_JS_BACKEND/apps/starnavigationapi/src/app/config/http/configuration.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-/**
- * Contains timeout and maxredirect arguments for http request 
- */
-export default registerAs('http', () => ({
-    timeout: process.env.HTTP_TIMEOUT,
-    maxRedirects: process.env.HTTP_MAX_REDIRECTS
-}));
