5b9d72d42b1b087c0462cc66f9660ef224a5e71e
sushilbailiwicksolution <sushil@bailiwicksolution.com>
2023-07-31 14:17:19 +0530

    updating old nodeApi

--- clientApi/run_api.sh
+++
@@ -1,5 +0,0 @@
-cd /home/star/nodejs/nodejs-dev/clientApi-3378
-
-file_postfix=`date +'%Y%m%d%H'`
-
-nohup node index.js 1>>"logs/client_api_"$file_postfix".txt" 2>>"logs/client_api_"$file_postfix".txt" &
