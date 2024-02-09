37bc74f280e6213f14174720694e7bb2bb740a56
sushilbailiwicksolution <sushil@bailiwicksolution.com>
2024-02-09 18:58:05 +0530

    new nest api build

--- NEST_JS_BACKEND/apps/starnavigationapi/src/app/config/app/configuration.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-export default registerAs('app', () => ({
-    env: process.env.APP_ENV,
-    name: process.env.APP_NAME,
-    url: process.env.APP_URL,
-    port: process.env.APP_PORT,
-    frontendAppPath: process.env.FRONTEND_APP_PATH
-}));
